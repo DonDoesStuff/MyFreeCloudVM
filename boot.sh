0b1e6fb83c54a0fe30d02e3af9fb3d00d1a070fe
lgamerlive <61466169+lgamerlive@users.noreply.github.com>
2023-04-27 21:21:53 +0100

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo Note:
+echo This script requires some setup. Make sure you read the Readme.md!
+# download megadown script
+curl -L -o megadown https://raw.githubusercontent.com/tonikelope/megadown/master/megadown
+# make it executable
+chmod +x megadown
+# download the mega file using megadown
+./megadown 'https://mega.nz/file/Kl5HlSTD#P6WNrDQrypvHOARnnxdw426SXl2qsBHXWKLaFBcf28M'
+# rename the downloaded file to w10.qcow2
+mv 'Windows10 tao.qcow2' w10.qcow2
+# update and install packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y qemu-kvm libvirt-daemon qemu-system-x86_64
+# run the qemu virtual machine in the background
+qemu-system-x86_64 -m 4G -cpu max -smp 4 -usbdevice tablet -drive file=w10.qcow2 -display vnc=:1 -vga vmware &
+# download and extract ngrok
+curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz | tar -xz
+# read the token from ngrok_key.txt
+token=$(cat ngrok_key.txt)
+# run ngrok with the token and port 5901
+./ngrok --authtoken $token tcp 5901
