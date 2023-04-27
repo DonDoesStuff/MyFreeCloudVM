dbe7f89f9c3888f8bd6e2df22b1a8c598746fc37
lgamerlive <61466169+lgamerlive@users.noreply.github.com>
2023-04-27 22:10:56 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,18 +5,9 @@ echo This script requires some setup. Make sure you read the Readme.md!
 # update and install packages
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y qemu-kvm libvirt-daemon qemu-system-x86_64
-
-# download RapidDownloader
-RAPID_DOWNLOADER_URL="https://github.com/sidneys/RapidDownloader/releases/download/v0.6.1/rapid-downloader-0.6.1-linux-x64.zip"
-RAPID_DOWNLOADER_DIR="rapid-downloader"
-RAPID_DOWNLOADER_PATH="$RAPID_DOWNLOADER_DIR/rapid-downloader"
-wget $RAPID_DOWNLOADER_URL -O rapid-downloader.zip
-unzip rapid-downloader.zip -d $RAPID_DOWNLOADER_DIR
-
-# download the mega file using RapidDownloader
-URL="https://mega.nz/file/Kl5HlSTD#P6WNrDQrypvHOARnnxdw426SXl2qsBHXWKLaFBcf28M"
-OUTPUT_FILE="w10.qcow2"
-$RAPID_DOWNLOADER_PATH/rdcli download $URL -o $OUTPUT_FILE
+sudo apt-get update
+sudo apt-get install megatools
+megadl 'https://mega.nz/file/Kl5HlSTD#P6WNrDQrypvHOARnnxdw426SXl2qsBHXWKLaFBcf28M' -o w10.qcow2
 
 # run the qemu virtual machine in the background
 qemu-system-x86_64 -m 4G -cpu max -smp 4 -usbdevice tablet -drive file=w10.qcow2 -display vnc=:1 -vga vmware &
