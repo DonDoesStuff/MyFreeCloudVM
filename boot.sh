3525f5daf2842bb7258f406b876c80043d14faac
lgamerlive <61466169+lgamerlive@users.noreply.github.com>
2023-04-27 21:32:58 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo Note:
 echo This script requires some setup. Make sure you read the Readme.md!
-# download megadown script
+# update and install packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y qemu-kvm libvirt-daemon qemu-system-x86_64
+# download megadown
 sudo apt-get -y install megadown
 # download the mega file using megadown
 ./megadown 'https://mega.nz/file/Kl5HlSTD#P6WNrDQrypvHOARnnxdw426SXl2qsBHXWKLaFBcf28M'
 # rename the downloaded file to w10.qcow2
 mv 'Windows10 tao.qcow2' w10.qcow2
-# update and install packages
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y qemu-kvm libvirt-daemon qemu-system-x86_64
 # run the qemu virtual machine in the background
 qemu-system-x86_64 -m 4G -cpu max -smp 4 -usbdevice tablet -drive file=w10.qcow2 -display vnc=:1 -vga vmware &
 # download and extract ngrok
